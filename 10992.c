36d339fec33412c3d9e7aef2fe955ce52aafccff
Cranberrybread <96380777+Cranberrybread@users.noreply.github.com>
2021-12-26 03:31:37 +0900

    Add files via upload

---
+++ 10992.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void main() {
+	int num;
+	printf("숫자를 입력하세요: ");
+	scanf_s("%d", &num);
+
+	int i, j;
+
+	for (i = 1; i <= num - 1; i++) {
+
+		for (j = 1; j <= num - i; j++) {
+			printf(" ");
+		}
+		printf("*");
+		for (j = 1; j <= 2 * (i - 1) - 1; j++) {
+			printf(" ");
+		}
+		if (i >= 2) {
+			printf("*");
+		}
+
+		puts("");
+
+	}
+	for (i = 1; i <= 2 * num - 1; i++) {
+		printf("*");
+	}
+
+}
